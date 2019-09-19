12517dd516c76021e4545c3ff33dcfd03b07f000
BGBiao <weichuangxxb@gmail.com>
2019-09-19 17:35:34 +0800

    Create start-regionserver.sh

---
+++ templates/start-regionserver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+su - hadoop -c 'sh /opt/app/hadoop/sbin/hadoop-daemon.sh start journalnode'
+sleep 3
+su - hadoop -c 'sh /opt/app/hadoop/sbin/hadoop-daemon.sh start datanode'
+sleep 3
+
+su - hadoop -c 'sh /opt/app/hadoop/sbin/yarn-daemon.sh start nodemanager'
+sleep 3
+su - hadoop -c 'sh /opt/app/hbase/bin/hbase-daemon.sh start regionserver'
