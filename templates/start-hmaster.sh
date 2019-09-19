9a76ea4f5b5dbc11c05b1cf9deae2009278dc56e
BGBiao <weichuangxxb@gmail.com>
2019-09-19 17:36:15 +0800

    Create start-hmaster.sh

---
+++ templates/start-hmaster.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+su - hadoop -c 'sh /opt/app/hadoop/sbin/hadoop-daemon.sh start namenode'
+
+sleep 3
+su - hadoop -c 'sh /opt/app/hadoop/sbin/hadoop-daemon.sh start zkfc'
+sleep 3
+su - hadoop -c 'sh /opt/app/hadoop/sbin/yarn-daemon.sh start resourcemanager'
+sleep 3
+su - hadoop -c 'sh /opt/app/hbase/bin/hbase-daemon.sh start hmaster'
